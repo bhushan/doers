b201f354fd2c138ab3b918b9d2e0ce39b408ef27
Bhushan <bhushangaykawad@gmail.com>
2021-10-10 02:05:06 +0530

    layout improvements and removed dashboard page

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 require __DIR__ . '/auth.php';
 
 Route::middleware(['auth'])->group(function () {
